53bef55d942b98c002b905a597cc988881e90a76
unknown <xindong@XDWS.europe.corp.microsoft.com>
2015-02-13 12:43:26 +0100

    Add more shelves

--- src/scripts/components/ReactjsApp.js
+++ src/scripts/components/ReactjsApp.js
@@ -14,8 +14,16 @@ var Shelf = require('./Shelf');
 
 var imageURL = require('../../images/yeoman.png');
 
-var books = [{id:'1', name: 'A Story Book', year: 1990, author: 'Someone'},
+var shelves = [];
+
+var shelf1 = [{id:'1', name: 'A Story Book', year: 1990, author: 'Someone'},
   		{id:'2', name: 'Another Story Book', year: 1991, author: 'Someone else'}];
+
+var shelf2 = [{id:'1', name: 'A Magazine', year: 1990, author: 'Magaziner'},
+      {id:'2', name: 'Another Magazine', year: 1991, author: 'Maganizer'}];
+
+shelves.push(shelf1);
+shelves.push(shelf2);
 
 var ReactjsApp = React.createClass({
   getInitialState: function(){
@@ -28,7 +36,11 @@ var ReactjsApp = React.createClass({
     return (
       <div className='main'>
         <ReactTransitionGroup transitionName="fade">
-        	<Shelf books={books}/>
+          {
+              shelves.map(function(s, i){
+                return <Shelf books={s} />
+              })
+          }
         </ReactTransitionGroup>
       </div>
     );
